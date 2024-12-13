c140bfe866d685e7c04f48bbe04daabffa7bd04e
homebody00 <youth1291@gmail.com>
2024-12-13 12:39:12 +0900

    int a 변수 추가

--- WindowsFormsApp_241213/Form1.cs
+++ WindowsFormsApp_241213/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp_241213
             // MessageBox.Show("와ㅁㅊ벌써배고픔");
 
             textBox_print.Text = "이것은\r\n멀티라인\r\n텍스트박스\r\n인데요.";
-
+            int a = 1;
         }
     }
 }
